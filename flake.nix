c18daec16ee71029cdb82607b66aa7e7f1940f94
Luke Peterson <hazelnusse@gmail.com>
2026-05-11 05:30:05 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,11 @@
+# DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
+# Use `nix run .#write-flake` to regenerate it.
+{
+  outputs = inputs: import ./outputs.nix inputs;
+
+  inputs = {
+    flake-file.url = "github:vic/flake-file";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+  };
+}

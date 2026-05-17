8cec628b7a13a322febe244bf1b1b01e973439c4
Luke Peterson <hazelnusse@gmail.com>
2026-05-16 19:26:27 -0700

    Introduce nixvim.base, enable lsp-format and none-ls

--- modules/formatting.nix
+++ modules/formatting.nix
@@ -24,4 +24,42 @@
     };
     #pre-commit.settings.hooks.treefmt.enable = true;
   };
+
+  flake.modules.nixvim.base = {
+    plugins = {
+      lsp.keymaps.lspBuf."<space>f" = "format";
+
+      lsp-format = {
+        enable = true;
+        lspServersToEnable = "none";
+      };
+
+      none-ls = {
+        enable = true;
+        sources.formatting.nix_flake_fmt.enable = true;
+      };
+    };
+
+    keymaps = [
+      {
+        key = "<leader>a";
+        options.desc = "Toggle autoformatting";
+        action = inputs.nixvim.lib.nixvim.mkRaw ''
+          function()
+            local lsp_format = require("lsp-format")
+
+            lsp_format.disabled = not lsp_format.disabled
+
+            local message
+            if lsp_format.disabled then
+              message = "Autoformatting is off"
+            else
+              message = "Autoformatting is on"
+            end
+            vim.notify(message, vim.log.levels.INFO)
+          end
+        '';
+      }
+    ];
+  };
 }

9b5f0bdc3f1dd61df6471a164da9900bac9aaa01
Luke Peterson <hazelnusse@gmail.com>
2026-05-26 06:47:40 -0700

    Add treesitter and associated editing files

---
+++ modules/editing/syntax-highlighting.nix
@@ -0,0 +1,5 @@
+{
+  flake.modules.nixvim.base = {
+    plugins.treesitter.highlight.enable = true;
+  };
+}
---
+++ modules/nixvim/treesitter.nix
@@ -0,0 +1,5 @@
+{
+  flake.modules.nixvim.base = {
+    plugins.treesitter.enable = true;
+  };
+}

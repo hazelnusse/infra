5caa5b6f50c3593931f4009e82c6044195f0e607
Luke Peterson <hazelnusse@gmail.com>
2026-07-09 20:22:34 -0700

    add usbutils and wget to home-manager

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -55,9 +55,11 @@
         #telegram-desktop
         tmux
         tree
+        usbutils
         #unzip
         xclip
         ledger-live-desktop
+        wget
         # # You can also create simple shell scripts directly inside your
         # # configuration. For example, this adds a command 'my-hello' to your
         # # environment:

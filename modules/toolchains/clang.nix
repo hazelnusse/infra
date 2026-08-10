97d1625c8057aa85289fa173d64b4ad801209cd3
Luke Peterson <hazelnusse@gmail.com>
2026-08-10 13:07:53 -0700

    Rainbow-color [[attribute]] brackets in cpp files
    nvim-treesitter-cpp's bundled rainbow-delimiters query covers (), {},
    and most [] uses but misses attribute_declaration nodes.

--- modules/toolchains/clang.nix
+++ modules/toolchains/clang.nix
@@ -1,5 +1,15 @@
 {
   flake.modules.nixvim.base = {
     lsp.servers.clangd.enable = true;
+
+    # rainbow-delimiters' bundled cpp query doesn't cover [[attribute]] brackets
+    # (attribute_declaration), so extend it to nest-color those too.
+    extraFiles."after/queries/cpp/rainbow-delimiters.scm".text = ''
+      ; extends
+
+      (attribute_declaration
+        "[[" @delimiter
+        "]]" @delimiter @sentinel) @container
+    '';
   };
 }

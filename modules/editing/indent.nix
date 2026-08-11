13bd417e791337c0f3d8d9c9907984db92f51955
Luke Peterson <hazelnusse@gmail.com>
2026-08-10 23:26:27 -0700

    Disable treesitter indent for C/C++
    nvim-treesitter's cpp indents.scm is thin (inherits from c,
    adds 3 rules) and misindents templates, member-init lists,
    and lambdas on Enter. Fall back to Neovim's built-in
    cindent-based indent/cpp.vim instead.

--- modules/editing/indent.nix
+++ modules/editing/indent.nix
@@ -1,5 +1,14 @@
 {
   flake.modules.nixvim.base = {
-    plugins.treesitter.indent.enable = true;
+    plugins.treesitter.indent = {
+      enable = true;
+      # nvim-treesitter's cpp/c indent queries are thin and misindent common
+      # constructs (templates, member-init lists, lambdas); Neovim's built-in
+      # cindent-based indent/cpp.vim is more reliable.
+      disable = [
+        "c"
+        "cpp"
+      ];
+    };
   };
 }

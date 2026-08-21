e958e95b5741dcea593916d97a814bac9f5109d8
Luke Peterson <hazelnusse@gmail.com>
2026-08-20 18:43:13 -0700

    Centralize the primary username into one option
    users.luke was hardcoded in three separate files. Add
    options.username (default "luke") and read it from each of them,
    so a future host needing a different account name is a one-line
    override instead of a three-file hunt.

--- modules/networking/daemon.nix
+++ modules/networking/daemon.nix
@@ -1,7 +1,8 @@
+{ config, ... }:
 {
   flake.modules.nixos.pc = {
     # TODO: look up Dawn's awesome sauce for networking
     networking.networkmanager.enable = true;
-    users.users.luke.extraGroups = [ "networkmanager" ];
+    users.users.${config.username}.extraGroups = [ "networkmanager" ];
   };
 }
--- modules/users/luke.nix
+++ modules/users/luke.nix
@@ -1,7 +1,8 @@
+{ config, ... }:
 {
   flake.modules.nixos.base = {
     # Define a user account. Don't forget to set a password with ‘passwd’.
-    users.users.luke = {
+    users.users.${config.username} = {
       isNormalUser = true;
       description = "Luke Peterson";
       extraGroups = [ "wheel" ];

0543f7fa8a4ca02835ab6557fcb62775b6a40e20
Luke Peterson <hazelnusse@gmail.com>
2026-09-22 23:10:11 -0700

    flake: regenerate flake.nix after input bump
    flake-file moved from vic/flake-file to denful/flake-file
    upstream, changing generated defaults (org URLs, nixpkgs
    tarball extension). check-flake-file failed because
    flake.nix was stale.

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
-# DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
+# DO-NOT-EDIT. This file was auto-generated using github:denful/flake-file.
 # Use `nix run .#write-flake` to regenerate it.
 {
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
@@ -16,12 +16,12 @@
       url = "github:mightyiam/files";
       flake = false;
     };
-    flake-file.url = "github:vic/flake-file";
+    flake-file.url = "github:denful/flake-file";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
-    import-tree.url = "github:vic/import-tree";
+    import-tree.url = "github:denful/import-tree";
     nix-wrapper-modules = {
       url = "github:BirdeeHub/nix-wrapper-modules";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -30,7 +30,7 @@
       url = "github:NixOS/nixos-hardware";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
     nixvim = {
       url = "github:nix-community/nixvim";
       flake = false;
